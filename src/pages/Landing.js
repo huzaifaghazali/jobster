2da03779ebf133dd814e753b7a664cbf2b273f81
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-25 12:25:20 +0500

    Set up the Landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import mainT from '../assets/images/Job-Seeker4.svg'
+
+function Landing() {
+  return (
+    <h1>Landing page</h1>
+  )
+}
+
+export default Landing
