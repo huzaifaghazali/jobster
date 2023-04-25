a7611889eef4ae98a4c822b34d26e2f0fdad100a
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-25 12:33:21 +0500

    Render elements on Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
-import mainT from '../assets/images/Job-Seeker4.svg'
+import logo from '../assets/images/logo.svg';
+// import main from '../assets/images/main.svg'
+import main from '../assets/images/Job-Seeker4.svg';
 
 function Landing() {
   return (
-    <h1>Landing page</h1>
-  )
+    <main>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+
+      <div className='container page'>
+        {/* info */}
+
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+
+          <p>
+            Edison bulb sustainable marxism hexagon, raw denim hell of affogato
+            pug four loko church-key austin. Bespoke selvage adaptogen, four
+            loko everyday carry kickstarter meditation yuccie. XOXO copper mug
+            pinterest, irony beard tattooed shoreditch coloring book shaman
+            chia.
+          </p>
+
+          <button className="btn btn-hero">
+            Login / Register
+          </button>
+        </div>
+
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
 }
 
-export default Landing
+export default Landing;
