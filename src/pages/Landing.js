9b06a677d6478d029ded901291f8c914d1fca245
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-26 12:13:57 +0500

    Create routes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
@@ -28,7 +29,7 @@ function Landing() {
             chia.
           </p>
 
-          <button className='btn btn-hero'>Login / Register</button>
+          <Link to='/register' className='btn btn-hero'>Login / Register</Link>
         </div>
 
         <img src={main} alt='job hunt' className='img main-img' />
