fea89e36106d93f6537b1e9e46db158930e1c263
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-27 12:47:33 +0500

    Create the FormRow component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -26,18 +26,29 @@ function Register() {
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            name='name'
-            value={values.name}
-            className='form-input'
-            onChange={handleChange}
-          />
-        </div>
+        {/* Name field */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+         {/* Email field */}
+         <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+         {/* Password field */}
+         <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
 
         <button type='submit' className='btn btn-block'>
           submit
