01df6bd8c84536ba44747ce2c541d73e7bf078d2
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-29 23:23:05 +0500

    Install react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h2>Navbar</h2>
+      <FaHome />
     </Wrapper>
   );
 };
