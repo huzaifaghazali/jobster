cb5adb8ae2a36e32d5c6e5ebc518038f8a9f865e
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-03 00:48:04 +0500

    Set logout message

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -45,11 +45,13 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
-    logoutUser: (state) => {
+    logoutUser: (state, { payload }) => {
       state.user = null;
       state.isSidebarOpen = false;
-      toast.success('Logout Successful!');
       removeUserToLocalStorage();
+      if(payload) {
+        toast.success(payload);
+      }
     },
   },
   extraReducers: {
