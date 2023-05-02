8ad1c377f60bc1613405fc620c0256d0a55a29d6
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-02 14:26:54 +0500

    Create clearValues reducer

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { FormRow } from '../../components';
 import FormRowSelect from '../../components/FormRowSelect';
-import { handleChange } from '../../features/job/jobSlice';
+import { handleChange, clearValues } from '../../features/job/jobSlice';
 
 const AddJob = () => {
   const {
@@ -88,7 +88,7 @@ const AddJob = () => {
             <button
               type='button'
               className='btn btn-block clear-btn'
-              onClick={() => console.log('clear values')}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
