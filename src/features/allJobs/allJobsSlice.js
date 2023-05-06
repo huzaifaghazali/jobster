2d1bab43eb0ead79fd996f491ea43a9f36c72810
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-06 16:45:48 +0500

    Remove showStats reducer bug

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -78,7 +78,7 @@ const allJobsSlice = createSlice({
     [showStats.fulfilled]: (state, { payload }) => {
       state.isLoading = false;
       state.stats = payload.defaultStats;
-      state.monthlyApplication = payload.monthlyApplication;
+      state.monthlyApplications = payload.monthlyApplications;
     },
     [showStats.rejected]: (state, { payload }) => {
       state.isLoading = false;
