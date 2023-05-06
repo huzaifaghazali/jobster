34aba908f04604e7d9a826e917a5db371675dbbe
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-06 18:33:54 +0500

    Set up the Chart container, Create Bar Chart and Area Chart component

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
 
 const ChartsContainer = () => {
+  const [bartChart, setBarChat] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
   return (
-    <h1>ChartsContainer</h1>
-  )
-}
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button onClick={() => setBarChat(!bartChart)}>
+        {bartChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {bartChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
+};
 
-export default ChartsContainer
+export default ChartsContainer;
