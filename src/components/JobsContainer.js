621090cdb988f111df50c170cf6d499f783380ad
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-06 23:10:09 +0500

    Set pagination setup

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -7,7 +7,7 @@ import { getAllJobs } from '../features/allJobs/allJobsSlice';
 import Wrapper from '../assets/wrappers/JobsContainer';
 
 const JobsContainer = () => {
-  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const { jobs, isLoading, page, totalJobs, numOfPages } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -29,7 +29,7 @@ const JobsContainer = () => {
  
   return (
     <Wrapper>
-      <h5>jobs info</h5>
+      <h5>{totalJobs} job{jobs.length > 1 && 's'} found</h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />
---
+++ src/components/PageBtnContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageBtnContainer = () => {
+  return (
+    <h1>PageBtnContainer</h1>
+  )
+}
+
+export default PageBtnContainer
