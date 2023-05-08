60ce982c03d1d61920d684537c7d0ad34193d670
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-08 20:21:08 +0500

    Create clearAllJobsState reducer

--- src/features/allJobs/allJobsThunk.js
+++ src/features/allJobs/allJobsThunk.js
@@ -13,7 +13,6 @@ export const getAllJobsThunk = async (_, thunkAPI) => {
 
   try {
     const resp = await customFetch.get(url);
-    console.log(resp.data);
     return resp.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.data.msg);
@@ -23,7 +22,6 @@ export const getAllJobsThunk = async (_, thunkAPI) => {
 export const showStatsThunk = async (_, thunkAPI) => {
   try {
     const resp = await customFetch.get('/jobs/stats');
-    console.log(resp.data);
     return resp.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.response.data.msg);
