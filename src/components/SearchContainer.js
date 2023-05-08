fa09e14c4be3eefb8b04c1ca255b2cc26621c3aa
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-05-08 23:15:41 +0500

    Add debounce funcationality in search container

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { FormRow, FormRowSelect } from '.';
@@ -6,21 +6,35 @@ import Wrapper from '../assets/wrappers/SearchContainer';
 import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
 
 const SearchContainer = () => {
-  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+  const [localSearch, setLocalSearch] = useState();
+  const { isLoading, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
     const { name, value } = e.target;
-    if(isLoading) return;
-    dispatch(handleChange({name, value})); // {name:e.target.name }
+    dispatch(handleChange({ name, value })); // {name:e.target.name }
+  };
+
+  const debounce = () => {
+    let timer;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 1000);
+    };
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(clearFilters())
+    setLocalSearch('')
+    dispatch(clearFilters());
   };
+
+  const optimizedDebounce = useMemo(() => debounce(), []);
 
   return (
     <Wrapper>
@@ -31,8 +45,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
 
           {/* Search by status */}
